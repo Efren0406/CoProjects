2028dd5711988dcdfe0261faa66d46d25324aff7
Michael Jiménez <mchllopezjimenez@gmail.com>
2022-07-11 22:54:50 -0500

    prueba

--- Chess/src/main.py
+++ Chess/src/main.py
@@ -3,6 +3,7 @@ import pieces
 import sys
 
 pygame.init()
+pygame.display.set_caption('Show Text')
 
 size = width, height = 800, 800
 margin = 60
@@ -13,6 +14,11 @@ screen = pygame.display.set_mode(size)
 brown_border = pygame.Color(51, 25, 0)
 brown_bold = pygame.Color(102, 51, 0)
 brown_light = pygame.Color(153, 76, 0)
+white_light = pygame.Color(255, 255, 255)
+
+# Letras
+font = pygame.font.Font('freesansbold.ttf', 30)
+text = font.render("A")
 
 # Dibujar Tablero
 box_width = (width - 2*margin)/8
@@ -23,7 +29,8 @@ for i in range(8):
         color = [brown_bold, brown_light]
         pygame.draw.rect(screen, color[i%2 + j%2 - 1], pygame.Rect(i * box_width + margin, j * box_height + margin, box_width, box_height))
 
-
+screen.blit(text, (10,10,20,20)) 
+        
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
