0a4f25af35667ea306c0187f20a59242198c8379
Efren0406 <efrenchito477@gmail.com>
2022-07-11 21:36:06 -0500

    Dibujar Tablero

--- Chess/src/main.py
+++ Chess/src/main.py
@@ -5,6 +5,7 @@ import sys
 pygame.init()
 
 size = width, height = 800, 800
+margin = 60
 
 screen = pygame.display.set_mode(size)
 
@@ -14,8 +15,14 @@ brown_bold = pygame.Color(102, 51, 0)
 brown_light = pygame.Color(153, 76, 0)
 
 # Dibujar Tablero
-def build_board(board):
-    pass
+box_width = (width - 2*margin)/8
+box_height = (height - 2*margin)/8
+
+for i in range(8):
+    for j in range(8):
+        color = [brown_bold, brown_light]
+        pygame.draw.rect(screen, color[i%2 + j%2 - 1], pygame.Rect(i * box_width + margin, j * box_height + margin, box_width, box_height))
+
 
 while True:
     for event in pygame.event.get():
--- Chess/src/pieces.py
+++ Chess/src/pieces.py
@@ -19,3 +19,79 @@ class Pawn():
         black_pawn = pygame.transform.scale(black_pawn, (width, height))
         color = [white_pawn, black_pawn]
         screen.blit(color[piece], (x, y))
+
+class Bishop():
+    def __init__(self):
+        self.x = 0
+        self. y = 0
+        self.widht = 0
+        self.height = 0
+
+    def draw(self, screen, x , y, width, height, piece = 0):
+        white_bishop = pygame.image.load(r'Chess\images\white_bishop.png')
+        white_bishop = pygame.transform.scale(white_bishop, (width, height))
+        black_bishop = pygame.image.load(r'Chess\images\black_bishop.png')
+        black_bishop = pygame.transform.scale(black_bishop, (width, height))
+        color = [white_bishop, black_bishop]
+        screen.blit(color[piece], (x, y))
+
+class Horse():
+    def __init__(self):
+        self.x = 0
+        self. y = 0
+        self.widht = 0
+        self.height = 0
+
+    def draw(self, screen, x , y, width, height, piece = 0):
+        white_horse = pygame.image.load(r'Chess\images\white_horse.png')
+        white_horse = pygame.transform.scale(white_horse, (width, height))
+        black_horse = pygame.image.load(r'Chess\images\black_horse.png')
+        black_horse = pygame.transform.scale(black_horse, (width, height))
+        color = [white_horse, black_horse]
+        screen.blit(color[piece], (x, y))
+
+class Tower():
+    def __init__(self):
+        self.x = 0
+        self. y = 0
+        self.widht = 0
+        self.height = 0
+
+    def draw(self, screen, x , y, width, height, piece = 0):
+        white_tower = pygame.image.load(r'Chess\images\white_tower.png')
+        white_tower = pygame.transform.scale(white_tower, (width, height))
+        black_tower = pygame.image.load(r'Chess\images\black_tower.png')
+        black_tower = pygame.transform.scale(black_tower, (width, height))
+        color = [white_tower, black_tower]
+        screen.blit(color[piece], (x, y))
+
+class Queen():
+    def __init__(self):
+        self.x = 0
+        self. y = 0
+        self.widht = 0
+        self.height = 0
+
+    def draw(self, screen, x , y, width, height, piece = 0):
+        white_queen = pygame.image.load(r'Chess\images\white_queen.png')
+        white_queen = pygame.transform.scale(white_queen, (width, height))
+        black_queen = pygame.image.load(r'Chess\images\black_queen.png')
+        black_queen = pygame.transform.scale(black_queen, (width, height))
+        color = [white_queen, black_queen]
+        screen.blit(color[piece], (x, y)) 
+        #TRIKITAKATELAS
+
+class Queen():
+    def __init__(self):
+        self.x = 0
+        self. y = 0
+        self.widht = 0
+        self.height = 0
+
+    def draw(self, screen, x , y, width, height, piece = 0):
+        white_queen = pygame.image.load(r'Chess\images\white_queen.png')
+        white_queen = pygame.transform.scale(white_queen, (width, height))
+        black_queen = pygame.image.load(r'Chess\images\black_queen.png')
+        black_queen = pygame.transform.scale(black_queen, (width, height))
+        color = [white_queen, black_queen]
+        screen.blit(color[piece], (x, y))
