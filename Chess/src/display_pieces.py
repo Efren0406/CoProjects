568c6c6a0e98c5a935df863a44064436a388b6ce
Efren0406 <efrenchito477@gmail.com>
2022-07-12 00:48:36 -0500

    Empezando con el movimiento de las piezas

--- Chess/src/display_pieces.py
+++ Chess/src/display_pieces.py
@@ -1,48 +1,65 @@
-from string import hexdigits
 import pygame
 import pygame
 from pieces import *
 
-def display_pieces(screen, box_width, box_height):
+def display_pieces(screen, box_width, box_height, board):
     width, height = pygame.display.get_window_size()
 
-def init_position(screen, box_width, box_height, margin):
+def init_position(screen, board, box_width, box_height, margin):
     width, height = pygame.display.get_window_size()
 
     # Piezas Blancas
     index = 0
     for pawn in white_pawns:
         pawn.draw(screen, index * box_width + margin, 6 * box_height + margin, box_width, box_height)
+        board[index][6] = 'wp' + str(index)
         index += 1
     
     white_bishops[0].draw(screen, 2 * box_width + margin, 7 * box_height + margin, box_width, box_height)
     white_bishops[1].draw(screen, 5 * box_width + margin, 7 * box_height + margin, box_width, box_height)
+    board[2][7] = 'wB1'
+    board[5][7] = 'wB2'
 
     white_horses[0].draw(screen, box_width + margin, 7 * box_height + margin, box_width, box_height)
     white_horses[1].draw(screen, 6 * box_width + margin, 7 * box_height + margin, box_width, box_height)
+    board[1][7] = 'wH1'
+    board[6][7] = 'wH1'
 
     white_towers[0].draw(screen, margin, 7 * box_height + margin, box_width, box_height)
     white_towers[1].draw(screen, 7 * box_width + margin, 7 * box_height + margin, box_width, box_height)
+    board[0][7] = 'wT1'
+    board[7][7] = 'wT2'
 
     white_queen.draw(screen, 3 * box_width + margin, 7 * box_height + margin, box_width, box_height)
+    board[3][7] = 'wQ'
 
     white_king.draw(screen, 4 * box_width + margin, 7 * box_height + margin, box_width, box_height)
+    board[4][7] = 'wK'
 
     # Piezas Negras
     index = 0
     for pawn in black_pawns:
         pawn.draw(screen, index * box_width + margin, box_height + margin, box_width, box_height)
+        board[index][1] = 'bp' + str(index)
         index += 1
 
     black_bishops[0].draw(screen, 2 * box_width + margin, margin, box_width, box_height)
     black_bishops[1].draw(screen, 5 * box_width + margin, margin, box_width, box_height)
+    board[2][0] = 'bB1'
+    board[5][0] = 'bB2'
 
     black_horses[0].draw(screen, box_width + margin, margin, box_width, box_height)
     black_horses[1].draw(screen, 6 * box_width + margin, margin, box_width, box_height)
+    board[1][0] = 'bH1'
+    board[6][0] = 'bH2'
 
     black_towers[0].draw(screen, margin, margin, box_width, box_height)
     black_towers[1].draw(screen, 7 * box_width + margin, margin, box_width, box_height)
+    board[0][0] = 'bT1'
+    board[7][0]
 
     black_queen.draw(screen, 3 * box_width + margin, margin, box_width, box_height)
+    board[3][0] = 'bQ'
 
     black_king.draw(screen, 4 * box_width + margin, margin, box_width, box_height)
+    board[4][0] = 'bK'
--- Chess/src/main.py
+++ Chess/src/main.py
@@ -10,6 +10,9 @@ margin = 60
 
 screen = pygame.display.set_mode(size)
 
+# Configuracion del tablero
+board = [['x' for i in range(8)] for j in range(8)]
+
 # Colores
 brown_border = pygame.Color(51, 25, 0)
 brown_bold = pygame.Color(102, 51, 0)
@@ -18,7 +21,7 @@ white_light = pygame.Color(255, 255, 255)
 
 # Letras
 font = pygame.font.Font('freesansbold.ttf', 30)
-text = font.render("A")
+text = font.render("A", False, (0, 0, 0))
 
 # Dibujar Tablero
 box_width = (width - 2*margin)/8
@@ -29,17 +32,20 @@ for i in range(8):
         color = [brown_bold, brown_light]
         pygame.draw.rect(screen, color[i%2 + j%2 - 1], pygame.Rect(i * box_width + margin, j * box_height + margin, box_width, box_height))
 
-<<<<<<< HEAD
+# <<<<<<< HEAD
 screen.blit(text, (10,10,20,20)) 
         
-=======
-init_position(screen, box_width, box_height, margin)
+# =======
+init_position(screen, board, box_width, box_height, margin)
 
->>>>>>> 04dbe2be9ea4406c28e7bcab85272d70d411228f
+# >>>>>>> 04dbe2be9ea4406c28e7bcab85272d70d411228f
 while True:
     for event in pygame.event.get():
+        mouse_position = pygame.mouse.get_pos()
         if event.type == pygame.QUIT:
             sys.exit()
-    
+        if event.type == pygame.MOUSEBUTTONUP:
+            i = int((mouse_position[0] - margin)//(width/8))
+            j = int((mouse_position[1] - margin)//(height/8))
 
     pygame.display.flip()
