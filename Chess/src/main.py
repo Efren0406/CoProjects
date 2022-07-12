5d85ded5b011ae200e059333724ac23a3e7c6450
Efren0406 <efrenchito477@gmail.com>
2022-07-12 12:05:06 -0500

    Optimizando el codigo

--- Chess/src/main.py
+++ Chess/src/main.py
@@ -1,8 +1,6 @@
 from ast import Num
 import pygame
 import sys
-from display_pieces import *
-from selected_piece import *
 
 pygame.init()
 pygame.display.set_caption('Show Text')
@@ -23,54 +21,11 @@ white_light = pygame.Color(255, 255, 255)
 # Letras izquierda
 font = pygame.font.SysFont(None, 30)
 
-text = font.render('A', True, white_light)
-screen.blit(text, (30,90))
-
-text1 = font.render('B', True, white_light)
-screen.blit(text1, (30,170))
-
-text3 = font.render('C', True, white_light)
-screen.blit(text3, (30,260))
-
-text4 = font.render('D', True, white_light)
-screen.blit(text4, (30,345))
-
-text5 = font.render('E', True, white_light)
-screen.blit(text5, (30,430))
-
-text6 = font.render('F', True, white_light)
-screen.blit(text6, (30,515))
-
-text7 = font.render('G', True, white_light)
-screen.blit(text7, (30,600))
-
-text8 = font.render('H', True, white_light)
-screen.blit(text8, (30,690))
-
-# Numeros casillas 
-num = font.render('1', True, white_light)
-screen.blit(num, (90,760))
-
-num1 = font.render('2', True, white_light)
-screen.blit(num1, (175,760))
-
-num2 = font.render('3', True, white_light)
-screen.blit(num2, (265,760))
-
-num3 = font.render('4', True, white_light)
-screen.blit(num3, (350,760))
-
-num4 = font.render('5', True, white_light)
-screen.blit(num4, (435,760))
-
-num5 = font.render('6', True, white_light)
-screen.blit(num5, (520,760))
-
-num6 = font.render('7', True, white_light)
-screen.blit(num6, (610,760))
-
-num7 = font.render('8', True, white_light)
-screen.blit(num7, (690,760))
+for i in range(8):
+    text = font.render(chr(65 + i), True, white_light)
+    num = font.render(str(i + 1), True, white_light)
+    screen.blit(text, (30, i * 85.7142857143 + 90))
+    screen.blit(num, (i * 85.7142857143 + 90,760))
 
 # Dibujar Tablero
 box_width = (width - 2*margin)/8
